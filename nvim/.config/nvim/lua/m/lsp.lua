e7b14d5dc61073c391c6c24e0346add9472c651c
astrlux <37908451+ThePartyNeverEnds@users.noreply.github.com>
2021-10-15 12:50:22 -0700

    post_obfuscation

--- nvim/.config/nvim/lua/m/lsp.lua
+++ nvim/.config/nvim/lua/m/lsp.lua
@@ -19,7 +19,7 @@ require'lspconfig'.efm.setup {
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   signs = true,
   underline = true,
-  update_in_insert = false
+  update_in_insert = true
 })
 local signs = { -- Define diagnostic signs
   Error = '✗',
