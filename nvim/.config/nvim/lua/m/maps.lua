eccd5c78620e978cecb6f866e8481769e2733a5b
astrlux <37908451+astrlux@users.noreply.github.com>
2021-10-16 21:45:11 -0700

    post_obfuscation

--- nvim/.config/nvim/lua/m/maps.lua
+++ nvim/.config/nvim/lua/m/maps.lua
@@ -19,9 +19,6 @@ map('v', '<', '<gv', nor)
 map('v', '>', '>gv', nor)
 -- Auto-close brackets
 map('i', '"', '""<left>', nor)
-map('i', '\'', '\'\'<left>', nor)
-map('i', '[', '[]<left>', nor)
-map('i', '(', '()<left>', nor)
 map('i', '{<CR>', '{<CR>}<ESC><S-O>', nor)
 -- Move cursor faster
 map('n', '<DOWN>', '8j', nor)
