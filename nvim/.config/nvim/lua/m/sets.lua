0b737c54d7f9c116f20e7265325b954bd53a2aba
astrlux <37908451+astrlux@users.noreply.github.com>
2021-10-17 23:30:46 -0700

    post_obfuscation

--- nvim/.config/nvim/lua/m/sets.lua
+++ nvim/.config/nvim/lua/m/sets.lua
@@ -6,7 +6,6 @@ o.showcmd = false
 o.showmode = false
 o.cmdheight = 1
 o.laststatus = 0
--- o.scrolloff = 7
 o.tabstop = 2
 o.shiftwidth = 2
 o.softtabstop = 2
@@ -36,6 +35,11 @@ function! MyHighlights() abort
   hi PmenuSel guibg=#2A2A2A
   hi EndOfBuffer ctermbg=black ctermfg=black
 
+  hi DiffAdd    guibg=green  guifg=black
+  hi DiffDelete guibg=red    guifg=black
+  hi DiffChange guibg=orange guifg=black 
+  hi DiffText   guibg=yellow guifg=black
+
   hi LspDiagnosticsSignHint guifg=orange
   hi LspDiagnosticsFloatingHint guifg=orange
   hi LspDiagnosticsVirtualTextHint guifg=orange guibg=#4D2A00
@@ -48,7 +52,7 @@ function! MyHighlights() abort
   hi LspDiagnosticsFloatingError guifg=red
   hi LspDiagnosticsVirtualTextError guifg=red guibg=#330000
 
-  hi TelescopeMatching guifg=NONE guibg=NONE
+  hi TelescopeMatching guifg=none guibg=none
   hi TelescopeSelection guibg=#2A2A2A
 endfunction
 augroup MyColors
