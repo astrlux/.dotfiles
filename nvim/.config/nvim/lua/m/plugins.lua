f430de885417f7d4a5b15c555cadf61ba3288306
astrlux <37908451+astrlux@users.noreply.github.com>
2021-10-23 20:22:40 -0700

    post_obfuscation

--- nvim/.config/nvim/lua/m/plugins.lua
+++ nvim/.config/nvim/lua/m/plugins.lua
@@ -12,7 +12,6 @@ vim.api.nvim_exec([[
 local use = require('packer').use
 require('packer').startup(function()
   use 'wbthomason/packer.nvim' -- Package manager
-  use 'hoob3rt/lualine.nvim' -- Status line
   use { -- Syntax highlighting
     'nvim-treesitter/nvim-treesitter',
     run = ':TSUpdate'
--- nvim/.config/nvim/lua/m/sets.lua
+++ nvim/.config/nvim/lua/m/sets.lua
@@ -14,7 +14,6 @@ o.expandtab = true
 o.smarttab = true
 o.signcolumn = 'number'
 o.nu = true
-o.relativenumber = true
 o.updatetime = 15
 o.timeoutlen = 500
 o.smartcase = true
@@ -30,9 +29,9 @@ o.background = 'dark'
 
 cmd [[
 function! MyHighlights() abort
-  hi LineNr guifg=#555555
+  "hi LineNr guifg=#555555
 
-  hi Visual guibg=#333222
+  hi Visual guibg=#555444
   hi VertSplit guifg=black guibg=#2A2A2A
 
   hi Pmenu guibg=#222222
