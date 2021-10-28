0bfc7151083b6257acbd6fcc9b1046f23f932992
astrlux <37908451+astrlux@users.noreply.github.com>
2021-10-27 23:45:25 -0700

    post_obfuscation

--- nvim/.config/nvim/lua/m/tree.lua
+++ nvim/.config/nvim/lua/m/tree.lua
@@ -7,15 +7,14 @@ require'nvim-tree'.setup {
   -- hijack netrw window on startup
   hijack_netrw = true,
   -- open the tree when running this setup function
-  open_on_setup = true,
+  open_on_setup = false,
   -- will not open on setup if the filetype is in this list
   ignore_ft_on_setup = {},
   -- closes neovim automatically when the tree is the last **WINDOW** in the view
-  auto_close = true,
-  auto_open = true,
+  auto_close = false,
   -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-  open_on_tab = false,
-  close_on_open = false,
+  open_on_tab = true,
+  close_on_open = true,
   -- hijack the cursor in the tree to put it at the start of the filename
   hijack_cursor = true,
   -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
