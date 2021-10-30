62191feb5309ae42c0e22ecc5b0caa9a5bfebb56
astrlux <37908451+astrlux@users.noreply.github.com>
2021-10-29 20:19:29 -0700

    post_obfuscation

--- nvim/.config/nvim/lua/m/sets.lua
+++ nvim/.config/nvim/lua/m/sets.lua
@@ -31,48 +31,48 @@ o.undofile = true
 o.termguicolors = true
 o.background = 'dark'
 
--- cmd [[
--- function! IndustryCustoms() abort
---   hi Visual guibg=#454545
---   hi VertSplit guifg=black guibg=black
---   hi Pmenu guibg=#222222
---   hi PmenuSel guibg=#333333
---   hi EndOfBuffer guibg=black guifg=black
---   hi LspDiagnosticsSignWarning guifg=orange
---   hi LspDiagnosticsFloatingWarning guifg=orange
---   hi LspDiagnosticsVirtualTextWarning guifg=orange guibg=#4D2A00
---   hi LspDiagnosticsSignHint guifg=yellow
---   hi LspDiagnosticsFloatingHint guifg=yellow
---   hi LspDiagnosticsVirtualTextHint guifg=yellow guibg=#4D2A00
---   hi LspDiagnosticsSignInformation guifg=purple
---   hi LspDiagnosticsFloatingInformation guifg=purple
---   hi LspDiagnosticsVirtualTextInformation guifg=#301934
---   hi LspDiagnosticsSignError guifg=red
---   hi LspDiagnosticsFloatingError guifg=red
---   hi LspDiagnosticsVirtualTextError guifg=red guibg=#330000
---   hi TelescopeMatching guifg=none guibg=none
---   hi TelescopeSelection guibg=#333222
--- endfunction
--- function! MelangeCustoms() abort
---   hi LspDiagnosticsSignWarning guifg=orange
---   hi LspDiagnosticsFloatingWarning guifg=orange
---   hi LspDiagnosticsVirtualTextWarning guifg=orange guibg=#4D2A00
---   hi LspDiagnosticsSignHint guifg=yellow
---   hi LspDiagnosticsFloatingHint guifg=yellow
---   hi LspDiagnosticsVirtualTextHint guifg=yellow guibg=#4D2A00
---   hi LspDiagnosticsSignInformation guifg=purple
---   hi LspDiagnosticsFloatingInformation guifg=purple
---   hi LspDiagnosticsVirtualTextInformation guifg=#301934
---   hi LspDiagnosticsSignError guifg=red
---   hi LspDiagnosticsFloatingError guifg=red
---   hi LspDiagnosticsVirtualTextError guifg=red guibg=#330000
---   hi TelescopeMatching guifg=none guibg=none
---   hi TelescopeSelection guibg=#444333
--- endfunction
--- augroup MyColors
---   autocmd!
---   autocmd ColorScheme industry call IndustryCustoms()
---   autocmd ColorScheme melange call MelangeCustoms()
--- augroup END
--- ]]
-cmd 'colorscheme melange'
+cmd [[
+function! IndustryCustoms() abort
+  hi Visual guibg=#454545
+  hi VertSplit guifg=black guibg=black
+  hi Pmenu guibg=#222222
+  hi PmenuSel guibg=#333333
+  hi EndOfBuffer guibg=black guifg=black
+  hi LspDiagnosticsSignWarning guifg=orange
+  hi LspDiagnosticsFloatingWarning guifg=orange
+  hi LspDiagnosticsVirtualTextWarning guifg=orange guibg=#4D2A00
+  hi LspDiagnosticsSignHint guifg=yellow
+  hi LspDiagnosticsFloatingHint guifg=yellow
+  hi LspDiagnosticsVirtualTextHint guifg=yellow guibg=#4D2A00
+  hi LspDiagnosticsSignInformation guifg=purple
+  hi LspDiagnosticsFloatingInformation guifg=purple
+  hi LspDiagnosticsVirtualTextInformation guifg=#301934
+  hi LspDiagnosticsSignError guifg=red
+  hi LspDiagnosticsFloatingError guifg=red
+  hi LspDiagnosticsVirtualTextError guifg=red guibg=#330000
+  hi TelescopeMatching guifg=none guibg=none
+  hi TelescopeSelection guibg=#333222
+endfunction
+function! MelangeCustoms() abort
+  hi LspDiagnosticsSignWarning guifg=orange
+  hi LspDiagnosticsFloatingWarning guifg=orange
+  hi LspDiagnosticsVirtualTextWarning guifg=orange guibg=#4D2A00
+  hi LspDiagnosticsSignHint guifg=yellow
+  hi LspDiagnosticsFloatingHint guifg=yellow
+  hi LspDiagnosticsVirtualTextHint guifg=yellow guibg=#4D2A00
+  hi LspDiagnosticsSignInformation guifg=purple
+  hi LspDiagnosticsFloatingInformation guifg=purple
+  hi LspDiagnosticsVirtualTextInformation guifg=#301934
+  hi LspDiagnosticsSignError guifg=red
+  hi LspDiagnosticsFloatingError guifg=red
+  hi LspDiagnosticsVirtualTextError guifg=red guibg=#330000
+  hi TelescopeMatching guifg=none guibg=none
+  hi TelescopeSelection guibg=#444333
+endfunction
+augroup MyColors
+  autocmd!
+  autocmd ColorScheme industry call IndustryCustoms()
+  autocmd ColorScheme melange call MelangeCustoms()
+augroup END
+]]
+cmd 'colorscheme gruvbox'
