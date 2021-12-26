0002d71c386784ddaea13c560faf97213e785c9c
astrlux <37908451+astrlux@users.noreply.github.com>
2021-12-26 12:59:49 -0800

    post_obfuscation

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,4 +1,4 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
-  hs.alert.show("Hello World!")
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'W', function()
+  hs.alert.show('Hello World!')
 end)
 
