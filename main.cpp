de2d22534c5d4eea1fd8d704894392b63e602757
astrlux <37908451+astrlux@users.noreply.github.com>
2021-12-31 18:24:38 -0800

    post_obfuscation

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include "/Users/astrlux/stdc++.h"
-
 using namespace std;
+class Solution {
+public:
+  int coinChange(vector<int> &coins, int amount) {
+    vector<int> needs(amount + 1, amount + 1);
+    needs[0] = 0;
+    for (auto c : coins) {
+      for (int a = c; a < amount; ++a) {
+        needs[a] = min(needs[a], needs[a - c] + 1);
+      }
+    }
+    return needs.back() > amount ? -1 : needs.back();
+  }
+};
 
-int main() {
-  IOS;
-  return 0;
-}
+/**
+topmost row as numbers 1..amount
+    init to 0 or multiple
+leftmost column as amounts
+    init to 0
+*/
