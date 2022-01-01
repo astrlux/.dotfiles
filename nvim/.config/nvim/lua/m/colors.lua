a9dced92648a9b7d185e4accff0a58ef88b96da6
astrlux <37908451+astrlux@users.noreply.github.com>
2021-12-31 17:12:12 -0800

    post_obfuscation

--- nvim/.config/nvim/lua/m/colors.lua
+++ nvim/.config/nvim/lua/m/colors.lua
@@ -1,5 +1,4 @@
-local o, g, cmd = vim.opt, vim.g, vim.cmd
+local o, _, cmd = vim.opt, vim.g, vim.cmd
 o.tgc = true -- termguicolors
 o.bg = 'dark' -- background [dark/light]
--- g.vscode_style = 'dark'
 cmd 'colorscheme gruvbox'
