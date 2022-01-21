cc6a67ba55748862ea177a11ca38636775188b5d
astrlux <37908451+astrlux@users.noreply.github.com>
2022-01-20 22:07:13 -0800

    post_obfuscation

--- nvim/.config/nvim/lua/m/colors.lua
+++ nvim/.config/nvim/lua/m/colors.lua
@@ -2,5 +2,5 @@ local o, g, cmd = vim.opt, vim.g, vim.cmd
 o.tgc = true -- termguicolors
 o.bg = 'dark'
 g.vscode_style = 'dark'
-g.material_style = 'darker'
-cmd 'colorscheme material'
+cmd 'colorscheme vscode'
+-- g.material_style = 'darker'
