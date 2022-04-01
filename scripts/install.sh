9ca807777916b809e2a65b9e242e6f0ba575ed5f
astrlux <37908451+astrlux@users.noreply.github.com>
2022-03-31 21:41:54 -0700

    post_obfuscation

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,10 +3,10 @@ mkdir -p ~/.config/nvim/lua
 mkdir -p ~/.config/nvim/plugin
 
 pushd $DOTFILES # path in zshrc
-  for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
-  do
-    echo "Installing $folder"
-    stow -D $folder
-    stow $folder
-  done
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+  echo "Installing $folder"
+  stow -D $folder
+  stow $folder
+done
 popd
